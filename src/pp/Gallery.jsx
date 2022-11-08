47215146c2e86fc8dadd40944780edea449f545a
Jessica Choi <jessicahchoi93@gmail.com>
2022-11-08 15:55:19 -0800

    image gallery

--- src/pp/Gallery.jsx
+++ src/pp/Gallery.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
+//import {GalleryData} from './GalleryData'
 import './gallery.css'
 
 function Gallery() {
   return (
     <div className='main-container'>
         <div className="gallery-container">
-           <div className="gallery-row">1</div>
-           <div className="gallery-row">2</div>
-           <div className="gallery-row">3</div>
-           <div className="gallery-row">4</div>
+            
         </div>
     </div>
   )
