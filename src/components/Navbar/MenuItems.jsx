03f7d552790ff721ba604bd86441fa467c80a4be
Jessica Choi <jessicahchoi93@gmail.com>
2023-03-10 23:15:48 -0500

    removed 'home' from navbar

--- src/components/Navbar/MenuItems.jsx
+++ src/components/Navbar/MenuItems.jsx
@@ -1,10 +1,5 @@
 export const MenuItems = [
     {
-        title:'Home',
-        url: '/',
-        cName: 'nav-links'
-    },
-    {
         title:'About',
         url: 'about',
         cName: 'nav-links'
